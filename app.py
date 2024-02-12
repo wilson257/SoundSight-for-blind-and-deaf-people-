faf37ad1d15a9f770dae4a214ae743052a93f3c3
wilson257 <wilsonaranha28@gmail.com>
2024-02-12 18:49:08 +0530

    Created Home page

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+  
+app = Flask(__name__) 
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/Blind',methods=['POST'])
+def Blind_btn():
+    if request.method=="Post":
+        return render_template('blind.html')
+    
+@app.route('/Deaf',methods=['POST'])
+def deaf_btn():
+    if request.method=="Post":
+        return render_template('deaf.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
